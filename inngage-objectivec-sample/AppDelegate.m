f20a769c723845fa7cf287e3ac5f0d58f535bb07
viniciusdepaula <viniciusdpaula@gmail.com>
2016-07-17 23:09:11 -0300

    React to Push Notification when app is closed

--- inngage-objectivec-sample/AppDelegate.m
+++ inngage-objectivec-sample/AppDelegate.m
@@ -33,8 +33,19 @@
     
     pushNotificationManager  = [[PushNotificationManager alloc]init];
     
+    NSDictionary* userInfo = [launchOptions valueForKey:@"UIApplicationLaunchOptionsRemoteNotificationKey"];
+    
+    NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
+    
+    if( [apsInfo objectForKey:@"alert"] != NULL) {
+        
+        [pushNotificationManager handlePushReceived:userInfo];
+    }
+    
     return YES;
 }
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -87,7 +98,6 @@
     didReceiveRemoteNotification:(NSDictionary *)userInfo {
     
     [pushNotificationManager handlePushReceived:userInfo];
-    
 }
 
 
